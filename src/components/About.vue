6b90abc064d887d4f02674a36cc08bf469173fa2
falue <info@fluescher.ch>
2020-12-20 19:18:20 +0100

    About identation

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,22 +1,22 @@
 <template>
-    <v-dialog model="dialog" max-width="600">
-      <v-card-title>
-        This is a silly website.
-      </v-card-title>
-      <v-card-text>
-      "In software engineering, rubber duck debugging is a method of debugging code. (...)
-      The name is a reference to a story in the book The Pragmatic Programmer in which a programmer would carry around a rubber duck and debug their code by forcing themselves to explain it, line-by-line, to the duck. (...)
-      In describing what the code is supposed to do and observing what it actually does, any incongruity between these two becomes apparent."
-      </v-card-text>
-      <v-card-subtitle>
-        From <a href="" target="_blank"></a>
-      </v-card-subtitle>
-      <v-card-actions>
-        <v-btn @click="dialog=false; $emit('close')">
-          Close
-        </v-btn>
-      </v-card-actions>
-    </v-dialog>
+  <v-dialog model="dialog" max-width="600">
+    <v-card-title>
+      This is a silly website.
+    </v-card-title>
+    <v-card-text>
+    "In software engineering, rubber duck debugging is a method of debugging code. (...)
+    The name is a reference to a story in the book The Pragmatic Programmer in which a programmer would carry around a rubber duck and debug their code by forcing themselves to explain it, line-by-line, to the duck. (...)
+    In describing what the code is supposed to do and observing what it actually does, any incongruity between these two becomes apparent."
+    </v-card-text>
+    <v-card-subtitle>
+      From <a href="https://en.wikipedia.org/wiki/Rubber_duck_debugging" target="_blank">wikipedia.org</a>
+    </v-card-subtitle>
+    <v-card-actions>
+      <v-btn @click="dialog=false; $emit('close')">
+        Close
+      </v-btn>
+    </v-card-actions>
+  </v-dialog>
 </template>
 
 <script>
@@ -27,7 +27,7 @@ export default {
 
   data () {
     return {
-      dialog:true,
+      dialog: true,
     }
   },
   watch: {
