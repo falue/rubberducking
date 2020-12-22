70cc2c8d20f21657d450b41fa84373ee354110b3
falue <info@fluescher.ch>
2020-12-22 12:15:35 +0100

    Footer text to seperate component

--- src/App.vue
+++ src/App.vue
@@ -8,17 +8,7 @@
     <!-- FOOTER -->
     <v-card-title class="justify-center fixed bottom caption grey--text fill-width" :class="$vuetify.breakpoint.smAndUp ? 'pb-6 mb-6' : ''">
       <div class="text-center">
-        Foolishly made by <a href="https://github.com/falue" target="_blank">Fabian Lüscher</a>
-        for you <v-icon x-small class="error--text px-1">mdi-heart</v-icon>
-        <br>
-        <a href="https://creativecommons.org/publicdomain/zero/1.0/" target="_blank" title="See creative commons license">CC0 1.0</a>
-        - 2020
-        <br>
-        Like this?
-        <a href="https://www.buymeacoffee.com/falue" target="_blank">
-          <v-icon x-small class="primary--text">mdi-coffee</v-icon>
-          Buy me a coffee
-        </a>
+        <Support/>  
       </div>
     </v-card-title>
     
@@ -26,10 +16,13 @@
 </template>
 
 <script>
+import Support from '@/components/Support'
+
 export default {
   name: 'App',
 
   components: {
+    Support
   },
 
   data: () => ({
---
+++ src/components/Support.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    Foolishly made by <a href="https://github.com/falue" target="_blank">Fabian Lüscher</a>
+    for you <v-icon x-small class="error--text px-1">mdi-heart</v-icon>
+    <br>
+    <a href="https://creativecommons.org/publicdomain/zero/1.0/" target="_blank" title="See creative commons license">CC0 1.0</a>
+    - 2020
+    <br>
+    Like this?
+    <a href="https://www.buymeacoffee.com/falue" target="_blank">
+      <v-icon x-small class="primary--text">mdi-coffee</v-icon>
+      Buy me a coffee
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Support',
+  props: {
+  },
+
+  data () {
+    return {
+    }
+  },
+  watch: {
+  },
+  computed: {
+  },
+  created() {
+  },
+  methods: {
+  }
+}
+</script>
